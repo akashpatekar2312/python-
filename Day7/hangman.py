44207d72683886404dbc11e7b840ec1ea097f12c
akashpatekar2312 <165651360+akashpatekar2312@users.noreply.github.com>
2024-06-01 02:34:08 +0530

    hangman.py

---
+++ Day7/hangman.py
@@ -0,0 +1,61 @@
+import random
+words = ["skynet","gensys"]
+hangman = ['''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========''']
+
+word_to_guess = random.choice(words)
+print (word_to_guess)
+dash = len(word_to_guess)
+dash_list = [""]
+for letters in range (dash) : 
+    dash_list += dash_list.extend('_')
+print (dash_list)
+
