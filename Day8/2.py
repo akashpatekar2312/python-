03b2983457314bb6ef8a5940a6b3a47608351dc3
akashpatekar2312 <165651360+akashpatekar2312@users.noreply.github.com>
2024-06-13 12:48:43 +0530

    2.py

---
+++ Day8/2.py
@@ -0,0 +1,7 @@
+# Functions with inputs 
+def greet(name):
+    print (f"Hello {name}")
+    print (f"How do you do {name}?")
+    print (f"Isn't the Wheather nice today {name} ?")
+
+greet("akash")
