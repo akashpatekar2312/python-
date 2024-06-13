25e1c670fc16beb47b0cdb81d0d9aaef57183d11
akashpatekar2312 <165651360+akashpatekar2312@users.noreply.github.com>
2024-06-13 12:49:05 +0530

    3.py

---
+++ Day8/3.py
@@ -0,0 +1,8 @@
+# Multiple parameter in a function
+
+def greet(name,location):
+    print(f"Hello {name}")
+    print (f"What is it like to live in {location}, {name}")
+    print (f"So {name} where do you live in {location}")
+
+greet(location = "pune" , name = "akash")
